0d06aa6686199501ddc4a2d44f62c5c1e8cd7b6f
ali <ali.saranj@gmail.com>
2024-01-17 20:23:26 +0330

    a

---
+++ TicTacTO.py
@@ -0,0 +1,59 @@
+class TicTacTo:
+    arrGameMap = [
+        ['1', '2', '3'],
+        ['4', '5', '6'],
+        ['7', '8', '9']
+    ]
+
+    def createMap(self):
+        return f"""
+        | {self.arrGameMap[0][0]} | {self.arrGameMap[0][1]} | {self.arrGameMap[0][2]} |
+        -------------
+        | {self.arrGameMap[1][0]} | {self.arrGameMap[1][1]} | {self.arrGameMap[1][2]} |
+        ------------
+        | {self.arrGameMap[2][0]} | {self.arrGameMap[2][1]} | {self.arrGameMap[2][2]} |
+        """
+
+    def showMap(self):
+        print(self.createMap())
+
+    def setOandX(self, value:str, character:chr):
+        if value == '1':
+            self.arrGameMap[0][0] = character
+        if value == '2':
+            self.arrGameMap[0][1] = character
+        if value == '3':
+            self.arrGameMap[0][2] = character
+        if value == '4':
+            self.arrGameMap[1][0] = character
+        if value == '5':
+            self.arrGameMap[1][1] = character
+        if value == '6':
+            self.arrGameMap[1][2] = character
+        if value == '7':
+            self.arrGameMap[2][0] = character
+        if value == '8':
+            self.arrGameMap[2][1] = character
+        if value == '9':
+            self.arrGameMap[2][2] = character
+
+    def winner(self):
+        if self.arrGameMap[0][0] == self.arrGameMap[0][1] == self.arrGameMap[0][2]:
+            return self.arrGameMap[0][0]
+        elif self.arrGameMap[1][0] == self.arrGameMap[1][1] == self.arrGameMap[1][2]:
+            return self.arrGameMap[1][0]
+        elif self.arrGameMap[2][0] == self.arrGameMap[2][1] == self.arrGameMap[2][2]:
+            return self.arrGameMap[2][0]
+        elif self.arrGameMap[0][0] == self.arrGameMap[1][1] == self.arrGameMap[2][2]:
+            return self.arrGameMap[0][0]
+        elif self.arrGameMap[0][2] == self.arrGameMap[1][1] == self.arrGameMap[2][0]:
+            return self.arrGameMap[0][2]
+        elif self.arrGameMap[0][0] == self.arrGameMap[1][0] == self.arrGameMap[2][0]:
+            return self.arrGameMap[0][0]
+        elif self.arrGameMap[0][1] == self.arrGameMap[1][1] == self.arrGameMap[2][1]:
+            return self.arrGameMap[0][1]
+        elif self.arrGameMap[0][2] == self.arrGameMap[1][2] == self.arrGameMap[2][2]:
+            return self.arrGameMap[0][2]
+
+
+
